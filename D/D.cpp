53a17981367934a6a601bf2a0c5ceb870c54571f
Nikolay Shirokih <nikolay.shirokih@gmail.com>
2021-09-12 17:36:37 +0400

    Добавьте файлы проекта.

---
+++ D/D.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <queue>
+#include <sstream>
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    int a, b;
+    size_t arg = 0;
+    a = atoi(argv[++arg]);
+    b = atoi(argv[++arg]);
+
+    vector<int> v(a * b);
+    istringstream ss(argv[++arg]);
+
+    for (int i = 0; i < a * b; i++)
+    {
+        char t;
+        ss >> t;
+        v[i] = t - '0';
+    }
+
+    auto t = v;
+    t.erase(unique(t.begin(), t.end()), t.end());
+    if (accumulate(t.begin(), t.end(), 0) == 1 && b > 1)
+        cout << "h ";
+    else
+        cout << "v ";
+    cout << accumulate(v.begin(), v.end(), 0);
+
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+bool f(double x)
+{
+	return x >= 50.2323435345;
+}
+
+int main()
+{
+	double a = 0;
+	double b = 100000;
+	double last = 0;
+	while (true)
+	{
+		cout << "123\n";
+		double mid = (a + b) / 2.;
+		if (mid == last)
+			break;
+		last = mid;
+		if (f(mid))
+			b = mid;
+		else
+			a = mid;
+	}
+	cout.setf(ios::fixed);
+	cout.precision(10);
+	cout << (a+b)/2. << endl;
+}
